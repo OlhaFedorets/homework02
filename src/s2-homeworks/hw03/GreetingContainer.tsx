b0548455a60cbc4d8b260c7acb8be74707b34df9
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-17 20:29:12 +0300

    change GreetingContainer.tsx

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -58,7 +58,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName: string = users[-1].name // need to fix
+    const lastUserName: string = users[users.length-1].name // need to fix
 
     return (
         <Greeting
