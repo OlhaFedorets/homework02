280781e8848d33ef137ba60f5289b761e50ef36a
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-26 13:44:43 +0300

    change homeWorkReducer.ts

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            action.payload === 'up'
+                ? state = state.sort(function (a, b) {
+                    return (a.name > b.name ? 1 : (a.name === b.name ? 0 : -1))
+                })
+                : state = state.sort(function (a, b) {
+                    return (a.name < b.name ? 1 : (a.name === b.name ? 0 : -1))
+                }) // need to fix
+            return state
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el=>el.age > 17) // need to fix
         }
         default:
             return state
