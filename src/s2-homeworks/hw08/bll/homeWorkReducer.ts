01b377273a2629c0dc906e43c9ab3e12b09c1eee
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-27 11:24:51 +0300

    change reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,11 +7,12 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+
             action.payload === 'up'
-                ? state = state.sort(function (a, b) {
+                ? state = [...state].sort(function (a, b) {
                     return (a.name > b.name ? 1 : (a.name === b.name ? 0 : -1))
                 })
-                : state = state.sort(function (a, b) {
+                : state = [...state].sort(function (a, b) {
                     return (a.name < b.name ? 1 : (a.name === b.name ? 0 : -1))
                 }) // need to fix
             return state
