6e9c0829a8e53f9bdf1a3d402b84e92d82c2fc93
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-16 13:13:17 +0300

    change SuperSort.tsx

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import downArrow from './../assets/free-icon-drop-down-arrow-60995.webp'
+import upArrow from './../assets/free-icon-up-arrow-12325100.webp'
+import upDownArrow from './../assets/free-icon-up-down-arrow-12153546.webp'
+
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = downArrow
+const upIcon = upArrow
+const noneIcon = upDownArrow
 
 export type SuperSortPropsType = {
     id?: string
@@ -14,8 +18,15 @@ export type SuperSortPropsType = {
 
 export const pureChange = (sort: string, down: string, up: string) => {
     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    // sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+    // return up // исправить
+    switch (sort) {
+        case down:
+            return up
+        case up:
+            return ''
+        default:
+            return down
+    }
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
@@ -42,12 +53,13 @@ const SuperSort: React.FC<SuperSortPropsType> = (
             onClick={onChangeCallback}
         >
             {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
+            <img
+                id={id + '-icon-' + sort}
+                src={icon}
+                style={{ width: '15px', height: '15px' }}
+            />
 
-            {icon} {/*а это убрать*/}
+            {/*{icon} /!*а это убрать*!/*/}
         </span>
     )
 }
